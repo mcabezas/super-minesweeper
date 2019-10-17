f5eca490164723db0956c530d943c7356ace1f77
mcabezas <mcabezas@outlook.com>
2019-10-17 07:26:49 -0300

    Initial version

---
+++ UI/Minesweeper.cs
@@ -0,0 +1,26 @@
+﻿using Minesweeper.Layer;
+using Minesweeper.Views;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI
+{
+    internal partial class Minesweeper : Form
+    {
+        private ArenaView arenaView;
+
+        public Minesweeper()
+        {
+            InitializeComponent();
+            ArenaView arenaView = new ArenaView();
+            this.Controls.Add(arenaView);
+        }
+   }
+}
---
+++ UI/Resources/ImageResource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Resources
+{
+    internal static class ImageResource
+    {
+        public static Image GetUnreveledBlankSquare(Size size) {
+            return GetImage(@"C:\Users\marcelo\source\repos\SuperMinesweeper\UI\Resources\Images\UnreveledBlankCell.PNG",
+                size);
+        }
+
+        public static Image GetReveledBlankSquare(Size size)
+        {
+            return GetImage(@"C:\Users\marcelo\source\repos\SuperMinesweeper\UI\Resources\Images\\ReveledBlankCell.PNG",
+                size);
+        }
+
+        private static Image GetImage(string imagePath, Size size)
+        {
+            Image rawImage = new Bitmap(imagePath);
+            return new Bitmap(rawImage, size);
+        }
+
+    }
+}
